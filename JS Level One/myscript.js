9423cead19437c22aa097161d84c40e591c86535
Dipesh <dipeshluitel40@gmail.com>
2025-09-30 11:32:42 +0545

    Simple JS struture learned

---
+++ JS Level One/myscript.js
@@ -0,0 +1,4 @@
+alert("Welcome to your Bank");
+var deposite = prompt("How much would you like to deposite?");
+alert("You've deposited: Rs." + deposite);
+console.log("You're a Hardworking man");
---
+++ JS Level One/part3_js.js
@@ -0,0 +1,4 @@
+var num = prompt("Enter the weight in pound");
+var kg = num * 0.454;
+alert("The " + num + " pounds equal to " + kg + " kg");
+console.log("Conversion Completes");
