160b299879e57c1068813d2e6aa731042c9eae5c
Dipesh <dipeshluitel40@gmail.com>
2025-10-10 23:04:28 +0545

    class deck hand and player created

--- Python_Level_Two/Project/CARD_GAME/war.py
+++ Python_Level_Two/Project/CARD_GAME/war.py
@@ -1,6 +1,34 @@
+from random import shuffle
 player1_name = input("Enter Player 1 Name: ")
 player2_name = input("Enter Player 2 Name: ")
-ranks = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
-suite = 'H D S C'.split()
+RANKS = '2 3 4 5 6 7 8 9 10 J Q K A'.split()
+SUITE = 'H D S C'.split()
 
-print(ranks)
+class Deck:
+    def __init__(self):
+        print("Creating New Ordered Deck")
+        self.allcards = [(s,r) for s in SUITE for r in RANKS]
+
+    def shuffle(self):
+        shuffle(self.allcards)
+
+    def split_half(self):
+        return(self.allcards[:26],self.allcards[26:])
+
+class Hand:
+    def __init__(self,cards):
+        self.cards = cards
+
+    def __str__(self):
+        return "Contain {} cards".format(len(self.cards))
+
+    def add(self,added_cards):
+        self.cards.extend(added_cards)
+
+    def remove_card(self):
+        return self.cards.pop()
+
+    
+card = Deck()
+card.shuffle()
+print(card.split_half())
