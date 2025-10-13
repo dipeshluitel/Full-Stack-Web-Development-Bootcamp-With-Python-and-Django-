b2fb318b534ffc59a955296216a7858edd6caa96
Dipesh <dipeshluitel40@gmail.com>
2025-10-13 19:42:48 +0545

    How to Actually Achieve MVT (follow txt along with comments)

--- Django_level_two/levelTwo/levelTwo/settings.py
+++ Django_level_two/levelTwo/levelTwo/settings.py
@@ -17,6 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -117,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
