96fa1207a41f1c24c019516e890dc61c47d5bb58
Dipesh <dipeshluitel40@gmail.com>
2025-10-16 12:46:07 +0545

    used template filter |title

--- Django_Level_Four/level_four/myApp/views.py
+++ Django_Level_Four/level_four/myApp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
+from datetime import datetime
 
 # Create your views here.
 def index(request):
-    return render(request,'myApp/index.html')
+    values = {'date': datetime.now()}
+    return render(request,'myApp/index.html',context=values)
 
 def other(request):
-    return render(request,'myApp/other.html')
+   string =  {"django" : "the web framework for web development"}
+   return render(request,'myApp/other.html',context= string)
 
 def relative(request):
     return render(request,'myApp/relative_url_templates.html')
