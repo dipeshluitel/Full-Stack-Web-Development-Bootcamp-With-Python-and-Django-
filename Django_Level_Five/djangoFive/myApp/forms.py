9e7f6d75c766c228d7ec9a3a38c1354293a9f948
Dipesh <dipeshluitel40@gmail.com>
2025-10-17 14:39:39 +0545

    directory of templates,static,media changed along with registration completion

--- Django_Level_Five/djangoFive/myApp/forms.py
+++ Django_Level_Five/djangoFive/myApp/forms.py
@@ -2,14 +2,14 @@ from django import forms
 from django.contrib.auth.models import User
 from myApp.models import UserProfileInfo
 
-class UserFrom(forms.ModelForm):
+class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta():
         model = User
         fields = ('username', 'email', 'password')
 
-class UserProfileInfo(forms.ModelForm):
+class UserProfileInfoForm(forms.ModelForm):
     class Meta():
         model = UserProfileInfo
         fields = ('portfolio_site','profile_pic')
