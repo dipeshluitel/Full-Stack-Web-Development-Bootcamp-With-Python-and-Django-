64843f4b343ae517ea71ec8f80f76b4a9d08a00d
Dipesh <dipeshluitel40@gmail.com>
2025-10-17 19:49:07 +0545

    four diffent modules which are required for logins imported inside views.py

--- Django_Level_Five/djangoFive/djangoFive/settings.py
+++ Django_Level_Five/djangoFive/djangoFive/settings.py
@@ -134,6 +134,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
+LOGIN_URL = '/myApp/user_login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
