71d994c3f249d232189cb13dd0edc3b55aa68f6e
Dipesh <dipeshluitel40@gmail.com>
2025-10-17 11:24:19 +0545

    models created and migrated and created base template

--- Django_Level_Five/djangoFive/djangoFive/settings.py
+++ Django_Level_Five/djangoFive/djangoFive/settings.py
@@ -127,6 +127,13 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
+
+MEDIA_ROOT = MEDIA_DIR
+MEDIA_URL = '/media/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
