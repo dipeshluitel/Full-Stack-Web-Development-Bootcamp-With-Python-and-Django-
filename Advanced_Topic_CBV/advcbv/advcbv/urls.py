55c35374bd0896dd32573bf4d16d55462e1df79d
Dipesh <dipeshluitel40@gmail.com>
2025-10-19 12:48:41 +0545

    simple class based view created response with HttpResponse

--- Advanced_Topic_CBV/advcbv/advcbv/urls.py
+++ Advanced_Topic_CBV/advcbv/advcbv/urls.py
@@ -19,6 +19,6 @@ from django.urls import path
 from basic_app import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.CBView.as_view(), name="index"),
     path('admin/', admin.site.urls),
 ]
--- Advanced_Topic_CBV/advcbv/basic_app/views.py
+++ Advanced_Topic_CBV/advcbv/basic_app/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.views.generic import View
 # Create your views here.
-def index(request):
-   return render(request, 'basic_app/index.html')
+
+# def index(request):
+#    return render(request, 'basic_app/index.html')
+
+class CBView(View):
+    def get(self,request):
+        return HttpResponse("Class Based Views are Awesome!")
