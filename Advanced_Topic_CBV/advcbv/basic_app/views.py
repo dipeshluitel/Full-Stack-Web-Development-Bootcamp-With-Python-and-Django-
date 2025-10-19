8ee64ce400e456f4e76463fed1106d66b01df88b
Dipesh <dipeshluitel40@gmail.com>
2025-10-19 13:59:26 +0545

    CBV with template view is created and injected data into template usint context

--- Advanced_Topic_CBV/advcbv/basic_app/views.py
+++ Advanced_Topic_CBV/advcbv/basic_app/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import View
+# from django.http import HttpResponse
+from django.views.generic import View, TemplateView
 # Create your views here.
 
 # def index(request):
 #    return render(request, 'basic_app/index.html')
 
-class CBView(View):
-    def get(self,request):
-        return HttpResponse("Class Based Views are Awesome!")
+# class CBView(View):
+#     def get(self,request):
+#         return HttpResponse("Class Based Views are Awesome!")
+
+class IndexView(TemplateView):
+    template_name = 'basic_app/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['injectme'] = 'BASIC INJECTIONS'
+        return context
